bde4be54def531d075a3b35974875b66f5423d5a
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-12 14:04:20 +0300

    Add GraphQL configuration in Gateway.

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'posts',
+              url: 'http://localhost:3001/graphql',
+            },
+            {
+              name: 'users',
+              url: 'http://localhost:3000/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
